ae9419217c86aa03fd850a26516a0e551777c106
Vitor Peçanha <vitor.pecanha@hotmail.com>
2021-10-13 10:00:00 -0300

    Add base do testes do cucumber

---
+++ features/step_definitions/cadastrar_plantas_step.rb
@@ -0,0 +1,27 @@
+  Dado('que estou na página de cadastro de planta') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('preencho o campo {string} com {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('clico em salvar') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('ela deve ser salvo no banco de dados do usuário') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('deverei ver ela nas minhas plantas cadastradas') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('não preencho o campo {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('deverei receber a mensagem de erro {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
