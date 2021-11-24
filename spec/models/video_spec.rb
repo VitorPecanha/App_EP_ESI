5d3f87998d116db2701f47869454d85de4c9d4fa
Vitor Contieri <vitor.pecanha@usp.br>
2021-11-24 20:45:29 -0300

    minor fix

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 require 'spec_helper'
+
 RSpec.describe Video, type: :model do
   it 'invalido sem nome da planta' do
     video = Video.new
     video.nome_do_video = "nome video"
     video.provedor = "provedor"
     video.link = "http://www.site.com.br"
-    expect(video).not_be be_valid
+    expect(video).not_to be_valid
   end
 end
