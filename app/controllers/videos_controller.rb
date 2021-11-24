7f8869d3a34a278afeaf723dc2b962c59e645914
Vitor Contieri <vitor.pecanha@usp.br>
2021-11-24 20:13:07 -0300

    Testes de save no bd

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,5 +3,13 @@ class VideosController < ApplicationController
   end
 
   def create
+    @video = Video.new(video_params)
+    @video.save
   end
+
+  private
+  def video_params
+    params.require(:video).permit(:nome_da_planta, :nome_do_video, :provedor, :link)
+  end
+
 end
